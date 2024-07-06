6fd398fe6a063cc1eab95126158ed4e364737074
Eduardo <eportugalpor@unsa.edu.pe>
2024-07-06 15:00:00 -0500

    Implementando vista UserRegister para manejar el registro de usuarios mediante POST, usando UserSerializer

--- BACKEND/CyberRoom_Dj/CyberManage/views.py
+++ BACKEND/CyberRoom_Dj/CyberManage/views.py
@@ -1,4 +1,8 @@
 from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import serializers
@@ -31,3 +35,13 @@ class CustomTokenObtainPairSerializer(serializers.Serializer):
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+class UserRegister(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
