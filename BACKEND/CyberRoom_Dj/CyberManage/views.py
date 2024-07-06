f9a5e21dd6d638091ccf6f3c14138900ff875065
Eduardo <eportugalpor@unsa.edu.pe>
2024-07-06 15:20:00 -0500

    Implementando vista UserLogout para manejar la revocación de tokens de sesión mediante POST

--- BACKEND/CyberRoom_Dj/CyberManage/views.py
+++ BACKEND/CyberRoom_Dj/CyberManage/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import serializers
@@ -45,3 +45,16 @@ class UserRegister(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLogout(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            print(str(e))
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
