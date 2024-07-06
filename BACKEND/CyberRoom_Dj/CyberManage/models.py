09269d5330ad210b4aaaf3a53ad4ba033206efd0
Eduardo <eportugalpor@unsa.edu.pe>
2024-07-06 13:20:00 -0500

    Definiendo el modelo User con campos personalizados y opciones de tipo de usuario. Implementa métodos de permisos y una propiedad para verificar el rol de administrador

--- BACKEND/CyberRoom_Dj/CyberManage/models.py
+++ BACKEND/CyberRoom_Dj/CyberManage/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractBaseUser):
+    ADMIN = 'admin'
+    EMPLOYEE = 'employee'
+    CLIENT = 'client'
+
+    USER_TYPE_CHOICES = [
+        (ADMIN, 'Administrador'),
+        (EMPLOYEE, 'Employee'),
+        (CLIENT, 'Client'),
+    ]
+    
+    username = models.CharField('Nombre de usuario', unique = True, max_length = 100)
+    email = models.EmailField('Correo Electrónico', max_length = 254, unique = True)
+    user_type = models.CharField(max_length=15, choices=USER_TYPE_CHOICES, default=CLIENT)
+    name = models.CharField('Nombres', max_length=200, blank = True, null = True)
+    lastName = models.CharField('Apellidos', max_length=200, blank = True, null = True)
+    imagen = models.CharField('Imagen de Perfil', max_length=200, blank = True, null = True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default = 0.00)
+    dni = models.CharField(max_length=20, blank = True, null = True)
+    phoneNumber = models.CharField(max_length=20, blank = True, null = True)
+    age = models.IntegerField(blank = True, null = True)
+    gender = models.CharField(max_length=10, blank = True, null = True)
+    registrationDate = models.DateField(auto_now_add=True)
+    usuario_activo = models.BooleanField(default = True)
+    usuario_administrador = models.BooleanField(default = False)
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+    
+    def __str__(self):
+        return f'{self.username},{self.lastName}'
+    
+    def has_perm(self,perm,obj = None):
+        return True
+    
+    def has_module_perms(self,app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.usuario_administrador
