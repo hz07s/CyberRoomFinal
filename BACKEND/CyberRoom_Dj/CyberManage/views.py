948bf480efe5636f80566f4d83fcc24bfc616dd3
Eduardo <eportugalpor@unsa.edu.pe>
2024-07-06 14:30:00 -0500

    Agregando vista CustomTokenObtainPairView para manejar la obtención de tokens JWT con un serializador personalizado

--- BACKEND/CyberRoom_Dj/CyberManage/views.py
+++ BACKEND/CyberRoom_Dj/CyberManage/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import (
+    User,
+)
+from .serializers import (
+    UserSerializer,
+)
 
-# Create your views here.
+# USER API's VIEW
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
