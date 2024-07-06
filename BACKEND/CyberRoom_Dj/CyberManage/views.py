3f9a0740ad49edc5ebf6b423c1c7c07d0f30969e
Eduardo <eportugalpor@unsa.edu.pe>
2024-07-06 14:50:00 -0500

    Implementando CustomTokenObtainPairSerializer para autenticación de usuarios y generación de tokens JWT, incluyendo el tipo de usuario en el payload

--- BACKEND/CyberRoom_Dj/CyberManage/views.py
+++ BACKEND/CyberRoom_Dj/CyberManage/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import authenticate
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import serializers
 from .models import (
     User,
 )
@@ -8,6 +11,23 @@ from .serializers import (
 
 # USER API's VIEW
 
+class CustomTokenObtainPairSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        user = authenticate(username=attrs['username'], password=attrs['password'])
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            payload = {
+                'refresh': str(refresh),
+                'access': access_token,
+                'user_type': user.user_type  # Agregar tipo de usuario al payload
+            }
+            return payload
+        else:
+            raise serializers.ValidationError("Invalid credentials")
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
