984f5254721950f3a080ee6d0f71afa9916d06d1
Eduardo <eportugalpor@unsa.edu.pe>
2024-07-07 16:30:00 -0500

    Definiendo UserComponent con propiedades para manejar datos de usuario, credenciales y estado de edición

--- FRONTEND/CyberRoom_An/src/app/user/user.component.ts
+++ FRONTEND/CyberRoom_An/src/app/user/user.component.ts
@@ -1,11 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent {
+  user: User = {
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
+  credentials = {
+    username: '',
+    password: ''
+  };
+  editedUser: User = {
+    username: '',
+    email: '',
+    name: '',
+    lastName: '',
+    imagen: '',
+    balance: 0,
+    dni: '',
+    phoneNumber: '',
+    age: 0,
+  };
+  refreshToken = '';
 
   constructor() { }
 
