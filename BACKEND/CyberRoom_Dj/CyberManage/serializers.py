f1560d267d56694ade4687cfd18fc28dd72568eb
Eduardo <eportugalpor@unsa.edu.pe>
2024-07-08 14:30:00 -0500

    Implementando UserManageCreateSerializer para la creación de usuarios con campos completos y contraseña en User

--- BACKEND/CyberRoom_Dj/CyberManage/serializers.py
+++ BACKEND/CyberRoom_Dj/CyberManage/serializers.py
@@ -22,3 +22,26 @@ class UserManageSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'user_type', 'name', 'lastName', 'imagen', 'balance', 'dni', 'phoneNumber', 'age', 'gender']
+
+class UserManageCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'user_type', 'name', 'lastName', 'imagen', 'balance', 'dni', 'phoneNumber', 'age', 'gender']
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def create(self, validated_data):
+        user = User.objects.create_total_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password'],
+            user_type=validated_data['user_type'],
+            name=validated_data.get('name', ''),
+            lastName=validated_data.get('lastName', ''),
+            imagen=validated_data.get('imagen', None),
+            balance=validated_data.get('balance', 0.00),
+            dni=validated_data.get('dni', ''),
+            phoneNumber=validated_data.get('phoneNumber', ''),
+            age=validated_data.get('age', None),
+            gender=validated_data.get('gender', '')
+        )
+        return user
