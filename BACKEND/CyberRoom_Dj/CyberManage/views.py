ea825881293990dd4a4d8dab87f30ae0d5db69f7
Eduardo <eportugalpor@unsa.edu.pe>
2024-07-08 15:00:00 -0500

    Implementando UserListApiView para listar usuarios usando UserManageSerializer

--- BACKEND/CyberRoom_Dj/CyberManage/views.py
+++ BACKEND/CyberRoom_Dj/CyberManage/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate
-from rest_framework import status
+from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -11,6 +11,8 @@ from .models import (
 )
 from .serializers import (
     UserSerializer,
+    UserManageSerializer,
+    UserManageCreateSerializer,
 )
 
 # USER API's VIEW
@@ -92,3 +94,7 @@ class UserCreateApiView(APIView):
         else:
             print("Errores de validación:", serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserListApiView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserManageSerializer
