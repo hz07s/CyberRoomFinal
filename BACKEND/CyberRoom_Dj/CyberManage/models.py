ec3ea505d952b2d2f785c80ffec3495b759a09bf
salasvictor22 <jsalasag@unsa.edu.pe>
2024-07-13 16:00:00 -0500

    Definiendo el modelo Tariff con campos personalizados.

--- BACKEND/CyberRoom_Dj/CyberManage/models.py
+++ BACKEND/CyberRoom_Dj/CyberManage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Tariff(models.Model):
+    id = models.AutoField(primary_key=True)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    tarifRange = models.CharField(max_length=15) 
+    
+    def __str__(self):
+        return f"{self.tarifRange} - Costo: {self.cost}"
