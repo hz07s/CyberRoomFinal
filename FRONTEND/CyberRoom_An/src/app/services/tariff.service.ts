ab2c21561eabb76f57e02efc0c191df7b9c1246c
salasvictor22 <jsalasag@unsa.edu.pe>
2024-07-13 17:50:00 -0500

    Se modifico el Servicio TariffService para manejar operaciones CRUD de tarifas en la API

--- FRONTEND/CyberRoom_An/src/app/services/tariff.service.ts
+++ FRONTEND/CyberRoom_An/src/app/services/tariff.service.ts
@@ -9,4 +9,23 @@ import { Tariff } from '../models/tariff';
 })
 
 export class TariffService {
+  private apiUrl = `${environment.apiBaseUrl}/tariffs`;  // Ajusta según la URL de tu API
+
+  constructor(private http: HttpClient) { }
+
+  getTariffs(): Observable<Tariff[]> {
+    return this.http.get<Tariff[]>(`${this.apiUrl}/list/`);
+  }
+
+  createTariff(tariff: Tariff): Observable<Tariff> {
+    return this.http.post<Tariff>(`${this.apiUrl}/create/`, tariff);
+  }
+
+  deleteTariff(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/${id}/`);
+  }
+
+  updateTariff(id: number, tariff: Tariff): Observable<Tariff> {
+    return this.http.put<Tariff>(`${this.apiUrl}/update/${id}/`, tariff);
+  }
 }
