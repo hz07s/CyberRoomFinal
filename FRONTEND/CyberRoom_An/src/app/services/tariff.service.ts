6404a3460ac43d2eb9c4f21060add2d07ac244d3
salasvictor22 <jsalasag@unsa.edu.pe>
2024-07-13 17:05:00 -0500

    Se creo el servicio para tariff en el proyecto de angular

---
+++ FRONTEND/CyberRoom_An/src/app/services/tariff.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Tariff } from '../models/tariff';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TariffService {
+}
