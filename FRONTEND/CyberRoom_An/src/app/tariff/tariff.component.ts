65055448052f13d2ef154c4c9c5b4a6988c7db35
salasvictor22 <jsalasag@unsa.edu.pe>
2024-07-13 17:10:00 -0500

    En el archivo tariff.component.ts se implemento TariffComponent con propiedades y métodos para manejar tarifas y el formulario

--- FRONTEND/CyberRoom_An/src/app/tariff/tariff.component.ts
+++ FRONTEND/CyberRoom_An/src/app/tariff/tariff.component.ts
@@ -6,5 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tariff.component.css']
 })
 export class TariffComponent implements OnInit {
+  tariffs: Tariff[] = [];
+  newTariff: Tariff = {
+    id: 0,
+    cost: 0,
+    tarifRange: ''
+  };
+  selectedTariff: Tariff | null = null; 
+  isEditing = false; 
 
+  // Nuevas propiedades para manejar el formulario
+  currentCost: number = 0;
+  currentTarifRange: string = '';
+
+  constructor(private tariffService: TariffService) {}
+
+  ngOnInit(): void {
+    this.getTariffs();
+  }
 }
