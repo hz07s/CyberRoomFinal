64bb1bec0100f5ec7682f9e4fec22dffb1721c77
salasvictor22 <jsalasag@unsa.edu.pe>
2024-07-15 19:32:00 -0500

    Se creo el componente machine en el proyecto de angular

---
+++ FRONTEND/CyberRoom_An/src/app/machine/machine.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { MachineService } from '../services/machine.service';
+import { Machine } from '../models/machine';
+
+@Component({
+  selector: 'app-machine',
+  templateUrl: './machine.component.html',
+  styleUrls: ['./machine.component.css']
+})
+export class MachineComponent implements OnInit {
+  
+}
