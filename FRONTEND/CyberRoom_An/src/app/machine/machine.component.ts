09409b1afd7c9b2750929d23f1647716dfd1c02c
salasvictor22 <jsalasag@unsa.edu.pe>
2024-07-15 19:47:00 -0500

    En el archivo machine.component.ts se implemento MachineComponent con propiedades y métodos para manejar máquinas y sus estados

--- FRONTEND/CyberRoom_An/src/app/machine/machine.component.ts
+++ FRONTEND/CyberRoom_An/src/app/machine/machine.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MachineService } from '../services/machine.service';
 import { Machine } from '../models/machine';
+import { TariffService } from '../services/tariff.service';
+import { Tariff } from '../models/tariff';
 
 @Component({
   selector: 'app-machine',
@@ -8,5 +10,23 @@ import { Machine } from '../models/machine';
   styleUrls: ['./machine.component.css']
 })
 export class MachineComponent implements OnInit {
-  
+  machines: any[] = [];
+  tariffs: Tariff[] = [];
+  machineStates = [
+    { value: 'available', label: 'Disponible' },
+    { value: 'booked', label: 'Reservada' },
+    { value: 'disabled', label: 'Deshabilitada' }
+  ];
+  newMachine: Machine = this.initializeMachine();
+  selectedMachine: Machine = this.initializeMachine();
+  isEditing = false;
+
+  get currentMachine(): Machine {
+    return this.isEditing ? this.selectedMachine : this.newMachine;
+  }
+
+  constructor(
+    private machineService: MachineService,
+    private tariffService: TariffService
+  ) {} 
 }
