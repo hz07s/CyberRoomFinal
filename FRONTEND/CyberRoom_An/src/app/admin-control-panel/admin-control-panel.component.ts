47e57d3cc4dadc9a3f31c2dff43648af7052d080
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-07-19 13:59:13 -0500

    frontend - agregando las funciones para las opciones de deshabilitar al admin

--- FRONTEND/CyberRoom_An/src/app/admin-control-panel/admin-control-panel.component.ts
+++ FRONTEND/CyberRoom_An/src/app/admin-control-panel/admin-control-panel.component.ts
@@ -8,3 +8,14 @@ import { Component } from '@angular/core';
 export class AdminControlPanelComponent {
   isActive = true; // Estado inicial del sitio web
   areReservationsEnabled = true; // Estado inicial de las reservas
+
+  toggleSiteStatus() {
+    this.isActive = !this.isActive;
+    // Lógica para habilitar/deshabilitar el sitio web
+  }
+
+  toggleReservationsStatus() {
+    this.areReservationsEnabled = !this.areReservationsEnabled;
+    // Lógica para habilitar/deshabilitar las reservas
+  }
+}
