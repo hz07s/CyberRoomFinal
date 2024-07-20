73ec7d0e3020b1a4fd994da58b01abf9f7e0e882
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-07-20 17:20:13 -0500

    frontend - en el ts de connected-users se hara uso de un servicio para obtener los usuarios connectados y segun eso mostrarlos

--- FRONTEND/CyberRoom_An/src/app/connected-users/connected-users.component.ts
+++ FRONTEND/CyberRoom_An/src/app/connected-users/connected-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnectedUsersService } from '../services/connected-users.service'; // Asegúrate de la ruta correcta
 
 @Component({
   selector: 'app-connected-users',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connected-users.component.css']
 })
 export class ConnectedUsersComponent implements OnInit {
+  connectedUsers: Array<{ id: number, name: string }> = []; // Define el tipo de datos para los usuarios de prueba
 
-  constructor() { }
+  constructor(private connectedUsersService: ConnectedUsersService) { }
 
   ngOnInit(): void {
+    // Aquí puedes agregar usuarios de prueba
+    this.connectedUsers = [
+      { id: 1, name: 'Usuario 1' },
+      { id: 2, name: 'Usuario 2' },
+      { id: 3, name: 'Usuario 3' }
+    ];
   }
-
 }
