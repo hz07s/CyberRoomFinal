9be7e11a09205cbd724d17ac43cc30b79e015af7
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-07-21 18:50:13 -0500

    backend - agregando las vistas para las transacciones incuyen los CRUDS

--- BACKEND/CyberRoom_Dj/CyberManage/views.py
+++ BACKEND/CyberRoom_Dj/CyberManage/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+from BACKEND.CyberRoom_Dj.CyberManage.models import Transaction
+from BACKEND.CyberRoom_Dj.CyberManage.serializers import TransactionSerializer
+
 # Create your views here.
+# TRANSACTION API's VIEW
+
+class TransactionCreateAPIView(generics.CreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+class TransactionListAPIView(generics.ListAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+class TransactionDeleteAPIView(generics.DestroyAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    lookup_field = 'id'
+
+class TransactionUpdateAPIView(generics.UpdateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    lookup_field = 'id'
